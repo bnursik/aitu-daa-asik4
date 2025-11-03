ae3548567acaa220a5d497355eefab4407ea6d33
bnursik <nbeusenbek@mail.ru>
2025-11-03 09:37:13 +0500

    feat:mvn and structure

---
+++ src/main/java/edu/youruni/smartgraph/App.java
@@ -0,0 +1,10 @@
+package edu.youruni.smartgraph;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
